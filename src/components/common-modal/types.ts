290d07c3ecc3b5e192fa9d62a9f2fcb2afe5684a
LouieJoy <louiemambusaoofficial@gmail.com>
2023-10-26 15:19:11 +0800

    integrate userlist table

--- src/components/common-modal/types.ts
+++ src/components/common-modal/types.ts
@@ -1,7 +1,10 @@
 import type { ReactNode } from "react";
 
+type ModalType = "default" | "success";
+
 export interface ModalProps {
   open: boolean;
   children?: ReactNode;
   onClose?: () => void;
+  modalType?: ModalType;
 }
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -4,6 +4,10 @@ import { UserList } from "./types";
 
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import CommonModal from "../components/common-modal/CommonModal";
 
@@ -23,10 +27,12 @@ import { getUserData } from "./utils";
 function Homepage() {
   const [userList, setUserList] = useState<UserList[]>([]);
   const [openModal, setOpenModal] = useState<boolean>(false);
+  const [openModalNotif, setOpenModalNotif] = useState<boolean>(false);
 
   const user: UserList = getUserData();
 
   const handleClose = () => setOpenModal(false);
+  const handleCloseNotif = () => setOpenModalNotif(false);
 
   function generateUser() {
     getUserList().then((res) => {
@@ -44,6 +50,7 @@ function Homepage() {
       },
     ]);
     setOpenModal(false);
+    setOpenModalNotif(true);
   }
 
   return (
@@ -57,15 +64,84 @@ function Homepage() {
           </StyledLargeButton>
         </Grid>
       </StyledBanner>
+
       <section id="table">
-        {userList.map((users, key) => {
-          return (
-            <ul key={key}>
-              <li>{users.name}</li>
-              <li>{users.email}</li>
-            </ul>
-          );
-        })}
+        <Grid p={10}>
+          <Grid display={"flex"} justifyContent={"space-between"} my={3}>
+            <Typography variant="h5" fontWeight={"bold"} color={"primary.main"}>
+              List of Selected Individual
+            </Typography>
+            <StyledButton variant="contained">Download All CV</StyledButton>
+          </Grid>
+          <Grid>
+            {userList.map((users, key) => {
+              return (
+                <Grid my={2}>
+                  <Accordion key={key}>
+                    <AccordionSummary>
+                      <Grid
+                        container
+                        display={"flex"}
+                        justifyContent={"space-between"}
+                        alignItems={"center"}
+                      >
+                        <Typography fontWeight={"bold"}>
+                          {users.name}
+                        </Typography>
+                        <StyledButton variant="contained">
+                          Download CV
+                        </StyledButton>
+                      </Grid>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Grid container>
+                        <Grid item xs={10}>
+                          <Grid
+                            item
+                            xs={7}
+                            sx={{
+                              display: "flex",
+                              flexDirection: "column",
+                            }}
+                          >
+                            <Grid mb={1}>
+                              <Typography variant="h6" fontWeight={"bold"}>
+                                Introduction
+                              </Typography>
+                              <Typography variant="body2">
+                                Lorem ipsum dolor sit amet consectetur,
+                                adipisicing elit. Reiciendis facere porro
+                                aliquid quibusdam ea eos exercitationem
+                                consequatur repudiandae. Nostrum necessitatibus
+                                dolor nesciunt vel molestias perferendis ullam
+                                dolores numquam voluptatem voluptate?
+                              </Typography>
+                            </Grid>
+                            <Grid mb={1}>
+                              <Typography variant="h6" fontWeight={"bold"}>
+                                Skills
+                              </Typography>
+                              <Typography variant="body2">HTML</Typography>
+                              <Typography variant="body2">CSS</Typography>
+                              <Typography variant="body2">
+                                Javascript / Typescript
+                              </Typography>
+                              <Typography variant="body2">Php</Typography>
+                              <Typography variant="body2">
+                                ReactJs / ReactTs
+                              </Typography>
+                              <Typography variant="body2">Laravel</Typography>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </AccordionDetails>
+                  </Accordion>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Grid>
       </section>
 
       <CommonModal open={openModal} onClose={handleClose}>
@@ -155,7 +231,6 @@ function Homepage() {
             </Grid>
           </Grid>
         </Grid>
-
         <Grid
           item
           xs={12}
@@ -173,6 +248,28 @@ function Homepage() {
             <StyledButton variant="contained" onClick={addUsertoList}>
               I like this one!
             </StyledButton>
+          </Grid>
+        </Grid>
+      </CommonModal>
+      <CommonModal open={openModalNotif} onClose={handleCloseNotif}>
+        <Grid
+          container
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Grid m={5}>
+            <Typography variant="h5" fontWeight={"bold"}>
+              Yey! Email sent to the individual!
+            </Typography>
+          </Grid>
+          <Grid m={2}>
+            <a href="#table">
+              <StyledButton variant="contained" onClick={handleCloseNotif}>
+                Proceed
+              </StyledButton>
+            </a>
           </Grid>
         </Grid>
       </CommonModal>
