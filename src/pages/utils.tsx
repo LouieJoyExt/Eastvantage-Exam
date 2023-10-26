17d097f3beb5c81d1d825fb36fb4a7b01477189d
LouieJoy <louiemambusaoofficial@gmail.com>
2023-10-26 08:09:03 +0800

    fix runtime issue

--- src/pages/utils.tsx
+++ src/pages/utils.tsx
@@ -2,15 +2,15 @@ import { UserList } from "./types";
 import { UserListResponse } from "../api/types";
 
 export const filteredUserData = (): UserList => {
-  const storedData: string | null = localStorage.getItem("userData");
-  const convertedUserData: UserListResponse = JSON.parse(storedData ?? "{}");
+  const storedData: string = localStorage.getItem("userData") ?? "'{}'";
+  const convertedUserData: UserListResponse = JSON.parse(storedData);
 
   const filteredData = {
     name: "",
     email: "",
   };
 
-  convertedUserData?.results.map((item) => {
+  convertedUserData.results.map((item) => {
     const name: string = `${item.name.title} ${item.name.first} ${item.name.last}`;
     const email: string = item.email;
 
